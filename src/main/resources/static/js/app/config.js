d64e78790f1d926ecff4f30d409e6f86f82b17b0
kBeliczynski <kamcio1975@gmail.com>
2020-09-17 01:37:34 +0200

    repair angular route and add kontakt.html

---
+++ src/main/resources/static/js/app/config.js
@@ -0,0 +1,17 @@
+angular.module('app')
+.config(function ($routeProvider) {
+    $routeProvider
+        .when('/start', {
+            templateUrl: 'js/app/start.html'
+           // controller: 'UserListController',
+           // controllerAs: 'ctrl'
+        })
+        .when('/kontakt', {
+            templateUrl: 'js/app/kontakt.html'
+           // controller: 'UserListController',
+           // controllerAs: 'ctrl'
+        })
+        .otherwise({
+            redirectTo: '/start'
+        });
+});
