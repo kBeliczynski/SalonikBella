4ce199d36855c634b4406ab374379b60e36a394d
kBeliczynski <kamcio1975@gmail.com>
2020-09-30 19:04:53 +0200

    zmiana zdjec w galeri dziala

--- src/main/resources/static/js/app/start/StartController.js
+++ src/main/resources/static/js/app/start/StartController.js
@@ -2,4 +2,34 @@ angular.module('app')
 .controller('StartController', ['Pictures', function(Pictures){
     var vm = this;
     vm.pictures = Pictures.getAll();
+
+    vm.leftPicture=0;
+    vm.centerPicture=1;
+    vm.rightPicture=2;
+
+    vm.prevPicture = function(){
+        if(vm.centerPicture == 1){
+            vm.rightPicture = vm.centerPicture;
+            vm.centerPicture = vm.leftPicture;
+            vm.leftPicture = vm.pictures.length-1;
+        }else{
+            vm.rightPicture = vm.centerPicture;
+            vm.centerPicture = vm.leftPicture;
+            vm.leftPicture--;
+        }
+    }
+
+    vm.nextPicture = function(){
+        if(vm.centerPicture == vm.pictures.length-2){
+            vm.leftPicture = vm.centerPicture;
+            vm.centerPicture = vm.rightPicture;
+            vm.rightPicture = 0;
+        }else{
+            vm.leftPicture = vm.centerPicture;
+            vm.centerPicture = vm.rightPicture;
+            vm.rightPicture++;
+        }
+    }
+
+
 }]);
