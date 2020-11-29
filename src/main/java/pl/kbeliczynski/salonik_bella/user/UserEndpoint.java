d87c2b348f292fd098f4f41f1c72146b87f47c1b
kBeliczynski <kamcio1975@gmail.com>
2020-11-29 14:35:28 +0100

    uroszczenie kodu rejestracji, teraz działa

--- src/main/java/pl/kbeliczynski/salonik_bella/user/UserEndpoint.java
+++ src/main/java/pl/kbeliczynski/salonik_bella/user/UserEndpoint.java
@@ -2,6 +2,7 @@ package pl.kbeliczynski.salonik_bella.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -41,7 +42,7 @@ public class UserEndpoint {
          }
      }
 
-    @PostMapping("/api/users")
+    @PostMapping(value = "/api/users", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> save(@RequestBody User user) {
         if(user.getId() == null) {
             user.setRole(userRoleRepository.findByRole("USER"));
--- src/main/resources/static/js/app/rejestracja/RegistrationController.js
+++ src/main/resources/static/js/app/rejestracja/RegistrationController.js
@@ -1,32 +1,14 @@
 angular.module('app')
-.controller('RegistrationController', function(Users, $location, $rootScope, FlashService) {
+.controller('RegistrationController', function($http, $resource, $location) {
     var vm = this;
+	var User = $resource('api/users');
+    vm.user = new User();
 
-    this.register = function() {
-        vm.dataLoading = true;
-        Users.Create(vm.user)
-            .then(function (response) {
-                if (response.success) {
-                    alert("udało się");
-                    FlashService.Success('Registration successful', true);
-                    $location.path('/zarejestrowano');
-                } else {
-                    alert("nie udało się");
-                    FlashService.Error(response.message);
-                    vm.dataLoading = false;
-                }
-            });
-    }
-
-    // .controller('RegistrationController', function(Users, User) {
-    //     var vm = this;
-    //     vm.user = new User();
-    //     vm.register = function() {
-    //         vm.user.role = 1;
-    //         vm.user.productList = [];
-    //         vm.user.perfumeList = [];
-    //         Users.add(vm.user);
-    //         vm.user = new User();
-    //     }
-    //}
+	vm.register = function(user) {
+		vm.user.$save(function(data) {
+			$location.path('/zarejestrowano');
+			vm.user = new User();
+		});
+	}
 });
+
