4b4d43393f9bef0a56500e95b023d7bf823a586f
kBeliczynski <kamcio1975@gmail.com>
2021-01-07 02:01:51 +0100

    działa wieksza część dodawania własnych wizyt. Brakuje zapisu do bazy

--- src/main/java/pl/kbeliczynski/salonik_bella/visitService/VisitEndpoint.java
+++ src/main/java/pl/kbeliczynski/salonik_bella/visitService/VisitEndpoint.java
@@ -2,6 +2,8 @@ package pl.kbeliczynski.salonik_bella.visitService;
 
 import org.springframework.aop.scope.ScopedProxyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -35,6 +37,21 @@ public class VisitEndpoint {
         return visitRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(value = "/api/visits")
+    public ResponseEntity<Visit> save(@RequestBody Visit visit) {
+        if(visit.getId() == null) {
+            Visit saved = visitRepository.save(visit);
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(saved.getId())
+                    .toUri();
+            return ResponseEntity.created(location).body(visit);
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 
     @DeleteMapping("/api/visits/{id}")
--- src/main/resources/static/js/app/wizyty/VisitsController.js
+++ src/main/resources/static/js/app/wizyty/VisitsController.js
@@ -70,10 +70,10 @@ angular.module('app')
         var Visit = $resource('api/visits');
         vm.visit = new Visit();
         vm.haircuts = Haircuts.getAll();
-        vm.mondayToFridayHours = [9,10,11,12,13,14,15,16];
-        vm.saturdayHours = [9,10,11,12,13];
+        vm.mondayToFridayHours = ['09','10','11','12','13','14','15','16'];
+        vm.saturdayHours = ['09','10','11','12','13'];
         vm.openTimeByDate = [];
-        vm.minutes = [0,10,20,30,40,50];
+        vm.minutes = ['00','10','20','30','40','50'];
         vm.pageMode = {MAIN: 'Main', FIND_DATE: 'FindDate', OWN_DATE: 'OwnDate'};
         vm.activePage = vm.pageMode.MAIN;
         vm.dateSummary = false;
@@ -95,7 +95,6 @@ angular.module('app')
 
         vm.searchNextDate = function () {   // wywoluje funkcje findDate(), szuka kolejnej dostepnej wizyty
             vm.actualSearchDate.setDate(vm.actualSearchDate.getDate()+1);
-        //.toJSON().slice(0,16).replace(/-/g,'/')
         }
 
         vm.findDate = async function (visit,date) {    // wywoluje funkcje checkDateAvailability(), szuka pierwszej dostepnej wizyty, ustawia zmienna findDateSummary gdy znajdzie wizyte
@@ -104,10 +103,14 @@ angular.module('app')
                 return;
             }
                 vm.incorrectData = false;
-                // edytuje zmienna openTimeByDate, wzgledem dnia wizyty
-                visit.visitBegin = '2021/01/06 15:00';
-                visit.visitEnd = '2021/01/06 16:00';
-                vm.openTimeByDate = vm.mondayToFridayHours;
+                while(1) {
+
+                    for (var hour of vm.openTimeByDate)
+                        for (var minute of vm.minutes)
+                            ;
+
+                }
+
                 await vm.checkDateAvailability(visit);
                 await console.log(vm.dateSummary);
                 await console.log(vm.actualSearchDate.toJSON().slice(0,16).replace(/-/g,'/'));
@@ -115,27 +118,71 @@ angular.module('app')
                 await console.log(vm.actualSearchDate.toJSON().slice(0,16).replace(/-/g,'/'));
         }
 
-        vm.checkDateAvailability = async function (visit) {    // sprawdza czy znaleziona wizyta nie koliduje z innymi wizytami
-            if(visit.visitBegin.substring(11,16) < '09:00' || visit.visitEnd.substring(11,16) > (vm.openTimeByDate[vm.openTimeByDate.length-1]+':50')) //  sprawdzy czy koniec i poczatek wizyty nie wykracza poza godzinę otwarcia i zamkniecia.
+        vm.findOwnDate = async function (visit) {
+            if(visit.haircutType == undefined || visit.phoneString == undefined || visit.minute == undefined || visit.hour == undefined || visit.date == undefined){
+                vm.incorrectData = true;
+                return;
+            }
+             vm.incorrectData = false;
+             await vm.setVisitBeginAndVisitEnd(visit,visit.date);
+             await vm.checkDateAvailability(visit);
+             if(vm.dateSummary) {
+                 visit.$save();
+             }
+
+        }
+
+        // sprawdza czy znaleziona wizyta nie koliduje z innymi wizytami
+        vm.checkDateAvailability = async function (visit) {
+            if(visit.visitBegin.substring(11,16) < '09:00' || visit.visitEnd.substring(11,16) > (vm.openTimeByDate[vm.openTimeByDate.length-1]+':50')) { //  sprawdzy czy koniec i poczatek wizyty nie wykracza poza godzinę otwarcia i zamkniecia.
                 vm.dateSummary = false;
-           await vm.searchByDate(visit.visitBegin.substring(0,10)).$promise.then( async visits => {
+                return;
+            }
+            var anyValue = vm.searchByDate(visit.visitBegin.substring(0,10));
+            await vm.searchByDate(visit.visitBegin.substring(0,10)).$promise.then( async visits => {
                 for await(var element of visits)
-                    if(!((visit.visitEnd <= element.visitBegin && visit.visitBegin <= element.visitBegin) || (visit.visitEnd >= element.visitEnd && visit.visitBegin >= element.visitEnd)))    // TUTAJ BEDZIE PETLA SPRAWDZAJACA CZY WARUNEK JEST SPELNIONY
+                    if(!((visit.visitEnd <= element.visitBegin && visit.visitBegin < element.visitBegin) || (visit.visitEnd > element.visitEnd && visit.visitBegin >= element.visitEnd))) {
                         vm.dateSummary = false;
-                vm.dateSummary = true;
+                        break;
+                    }else
+                        vm.dateSummary = true;
             })
+            if(anyValue === 0) // gdy nie ma wizyt w tym dniu, a wizyta mieści się między godzinami gdy salon jest otwarty, dodajemy ją
+                vm.dateSummary = true;
         }
 
         vm.acceptDate = function () {   // gdy data jest poprawna wysyła ją do akceptacj
 
         }
 
-        vm.setOpenTimeByDate = function () {  // sprawdza godziny otwarcia i ustawia zmienna openTimeByDate
-            //  sprawdza dzień w którym rezerwowana jest wizyta i wybiera tablice w ktorych rezerwowane mogą być wizyty
+        // ustawia prawidlowa datę wizyty, później według niej liczy początek wizyty, oraz koniec dodając czas usługi
+        vm.setVisitBeginAndVisitEnd = async function(visit, date){
+            await date.setHours(visit.hour);
+            await date.setHours(date.getHours()+1);   // dodaje 1 ponieważ mamy czas zimowy
+            await date.setMinutes(visit.minute);
+            visit.visitBegin = await date.toJSON().slice(0,16).replace(/-/g,'/');
+            var hour = await visit.haircutType/60;
+            var minute = await visit.haircutType%60;
+            await date.setHours(date.getHours()+hour);
+            await date.setMinutes(date.getMinutes()+minute);
+            visit.visitEnd = await date.toJSON().slice(0,16).replace(/-/g,'/');
         }
 
+        // sprawdza dzień w którym rezerwowana jest wizyta i wybiera tablice w ktorych rezerwowane mogą być wizyty. ustawia zmienna openTimeByDate
+        vm.setOpenTimeByDate = function (data) {
+            if(data === undefined)
+                return;
+            else if(data.getDay() === 0)   // czy niedziela
+                vm.openTimeByDate = [];
+            else if(data.getDay() === 6)   // czy sobota
+                vm.openTimeByDate = vm.saturdayHours;
+            else
+                vm.openTimeByDate = vm.mondayToFridayHours; // inny dzień
+        }
+
+        // zwraca wszystkie wizyty, których data jest równa zmiennej 'date'
         vm.searchByDate = date => {
             return Visits.getAll({date});
-     }
+        }
 
     });
